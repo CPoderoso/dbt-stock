c7585a9d785d9d0c2425e1179f42bbc099fee1b6
Celso Poderoso <cpoderoso@gmail.com>
2024-02-14 20:11:57 -0500

    Update publicFinance_dag.py

--- publicFinance_dag.py
+++ publicFinance_dag.py
@@ -1,16 +1,17 @@
-def personalFinance():
+def personalFinanceYahoo():
     import yfinance as fin
     import pandas as pd
     from datetime import date, timedelta, time
     from google.cloud import bigquery
 
     t = fin.Tickers( 'msft aapl amzn tsla' )
+    #df = t.history( start='2023-07-31', end='2023-08-05')
     df = t.history("1d")
     df1 = df.stack()
 
     #print(df1)
 
-    table_id ="<your Bigquery project and dataset>.stock"
+    table_id ="<your_bigqyeryProject>.stock"
     client = bigquery.Client()
 
     columns = list(df1)
@@ -41,6 +42,48 @@ def personalFinance():
     else:
         print("Encountered errors while inserting rows: {}".format(errors))
 
+def personalFinanceTransactions():
+    import os
+    import json
+    from google.cloud import bigquery
+
+
+    # Specify the path to the folder containing your JSON files
+    json_folder_path = "/mnt/c/Users/chpod/Google Drive/appsheet/data/PersonalYFinance-8046497/Files/"
+
+    # Initialize an empty dictionary to store the merged data
+    merged_data = []
+
+    # Iterate through each JSON file in the folder
+    for filename in os.listdir(json_folder_path):
+        if filename.endswith('.json'):
+            json_file_path = os.path.join(json_folder_path, filename)
+            with open(json_file_path, 'r') as json_file:
+                data = json.load(json_file)
+                merged_data.append(data)
+    if merged_data != []:
+        table_id ="cpoderoso-dtsc.dbt_cpoderoso.transactions"
+        client = bigquery.Client()
+
+        # print( merged_data )
+
+        errors = client.insert_rows_json(
+            table_id,
+            merged_data
+            )
+
+        if errors ==[]:
+            # Will delete JSON files only if the insert succeed
+            for filename in os.listdir(json_folder_path):
+                if filename.endswith('.json'):
+                    json_file_path = os.path.join(json_folder_path, filename)
+                    os.remove(json_file_path)
+            print("New rows have been added and CDC files deleted.")   
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
+    else:
+        print('No Transactions to add today')
+
 ################################
 
 from airflow import DAG
@@ -57,7 +100,7 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime(2023, 8, 9),
     'end_date': None,
-    'retries': 0,
+    'retries': 2,
     'retry_delay': timedelta(minutes=1),
 }
 
@@ -76,11 +119,19 @@ dag = DAG(
 )
 
 # Define the PythonOperator that runs your Python script
-run_python_script = PythonOperator(
-    task_id='run_python_script',
-    python_callable=personalFinance,
+run_Yahoo = PythonOperator(
+    task_id='run_Yahoo',
+    python_callable=personalFinanceYahoo,
+    dag=dag,
+)
+
+run_Transactions = PythonOperator(
+    task_id='run_Transactions',
+    python_callable=personalFinanceTransactions,
+    trigger_rule='all_success',
     dag=dag,
 )
 
 # Set the task dependency
-run_python_script
+run_Yahoo >> run_Transactions
+
